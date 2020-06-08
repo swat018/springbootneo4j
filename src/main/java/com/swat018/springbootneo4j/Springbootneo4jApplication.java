5f2b2b324c8d51a2978f91527a4eb719c62580a6
jinwoo park <swat018@gmail.com>
2020-06-09 04:41:09 +0900

    Initial commit

---
+++ src/main/java/com/swat018/springbootneo4j/Springbootneo4jApplication.java
@@ -0,0 +1,13 @@
+package com.swat018.springbootneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootneo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootneo4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swat018/springbootneo4j/Springbootneo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swat018.springbootneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootneo4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
